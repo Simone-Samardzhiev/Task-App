d3bd2738010fee7e918458f4b3821a47464e7dea
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-10-08 22:54:49 +0300

    Add LoginViewModleProtocol.

--- Task-App/Login/ViewModel/LoginViewModel.swift
+++ Task-App/Login/ViewModel/LoginViewModel.swift
@@ -8,6 +8,6 @@
 import Foundation
 
 @Observable
-class LoginViewModel: ObservableObject {
+class LoginViewModel: LoginViewModelProtocol {
     
 }
---
+++ Task-App/Login/ViewModel/LoginViewModelProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewModelProtocol.swift
+//  Task-App
+//
+//  Created by Simone Samardzhiev on 8.10.24.
+//
+
+import Foundation
+
+/// Protocol used to create login view model.
+protocol LoginViewModelProtocol {
+    
+}
