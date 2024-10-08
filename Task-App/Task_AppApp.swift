c5de1054ff4884e0f221bacf9fcf77e277be8ec7
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-10-08 14:19:21 +0300

    Initial Commit

---
+++ Task-App/Task_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_AppApp.swift
+//  Task-App
+//
+//  Created by Simone Samardzhiev on 8.10.24.
+//
+
+import SwiftUI
+
+@main
+struct Task_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
