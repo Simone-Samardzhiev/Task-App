f41b50d56e38b4ea6b85434b5ad67fe3f320242c
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-10-08 20:36:00 +0300

    Add LoginView

---
+++ Task-App/Login/View/LoginView.swift
@@ -0,0 +1,19 @@
+//
+//  LoginView.swift
+//  Task-App
+//
+//  Created by Simone Samardzhiev on 8.10.24.
+//
+
+import SwiftUI
+
+/// View used to log in.
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
