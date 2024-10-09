d709e989bed6c43b9d911a795a3078ac519d1fdf
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-10-09 17:46:16 +0300

    Add LoginButton.swift

---
+++ Task-App/Login/View/LoginButton.swift
@@ -0,0 +1,30 @@
+//
+//  LoginButton.swift
+//  Task-App
+//
+//  Created by Simone Samardzhiev on 9.10.24.
+//
+
+import SwiftUI
+
+
+/// Button used to login
+struct LoginButton: View {
+    /// Login view model.
+    @Environment(LoginViewModel.self) var loginViewModel
+    
+    var body: some View {
+        Button {
+            
+        } label: {
+            Text("Login")
+                .foregroundStyle(Color.primary)
+                .font(.largeTitle)
+                .padding()
+                .background {
+                    RoundedRectangle(cornerRadius: 10)
+                }
+        }
+        .padding(.horizontal, CGFloat.paddingValue)
+    }
+}
