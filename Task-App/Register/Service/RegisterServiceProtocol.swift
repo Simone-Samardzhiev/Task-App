0c197c4ef3e8ebc08e8dafff1f4ed68b57ba1631
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-10-09 13:38:40 +0300

    Add RegisterServiceProtocol.swift

--- Task-App/Register/Service/RegisterServiceProtocol.swift
+++ Task-App/Register/Service/RegisterServiceProtocol.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+
+/// Protocol used to define the requirements for register service.
+///
+/// The protocol define property for encoding json data.
+/// It also define method used to login the user.
+protocol RegisterServiceProtocol {
+    // MARK: Properties
+    
+    var encoder: JSONEncoder { get }
+    
+    // MARK: Methods
+    
+    /// Method that will register a user.
+    ///
+    /// The method will send the user email and password to the server.
+    /// If the registration was successful it won't thrown an error.
+    /// 
+    /// - Parameters:
+    ///   - email: The email of the user.
+    ///   - password: The password of the user.
+    func register(email: String, password: String) async throws
+}
