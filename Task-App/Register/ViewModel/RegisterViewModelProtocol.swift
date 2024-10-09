5ffdbcc3c881ef34f72c3385ec6053bc20a14da9
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-10-09 13:06:18 +0300

    Add RegisterViewModelProtocol.swift

---
+++ Task-App/Register/ViewModel/RegisterViewModelProtocol.swift
@@ -0,0 +1,34 @@
+//
+//  RegisterViewModelProtocol.swift
+//  Task-App
+//
+//  Created by Simone Samardzhiev on 9.10.24.
+//
+
+import Foundation
+
+
+/// Protocol used to define the requirements for register view model.
+///
+/// The protocol defines necessary properties for storing the email, password and confirm password.
+/// It also provide methods to check if the email is valid and if the password is strong enough. 
+protocol RegisterViewModelProtocol {
+    // MARK: Properties
+    
+    /// Variable storing the email of the user.
+    var email: String { get }
+    /// Variable storing the password of the user.
+    var password: String { get }
+    /// Variable storing the
+    var confirmPassword: String { get }
+    
+    // MARK: Methods
+    
+    /// Method that will check if the email is valid.
+    /// - Returns: True if the email is valid otherwise false.
+    func checkEmail() -> Bool
+    
+    /// Method that will check if the password is strong enough.
+    /// - Returns: True if the password is strong otherwise false.
+    func checkPassword() -> Bool
+}
