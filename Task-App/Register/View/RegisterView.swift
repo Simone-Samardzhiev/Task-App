ee8eb9305e5174b7c9d793c6bb54f16b83a32335
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-10-09 12:53:22 +0300

    Add RegisterView.swift

---
+++ Task-App/Register/View/RegisterView.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterView.swift
+//  Task-App
+//
+//  Created by Simone Samardzhiev on 9.10.24.
+//
+
+import SwiftUI
+
+
+/// View used to register 
+struct RegisterView: View {
+    var body: some View {
+        GeometryReader { geometry in
+            VStack {
+                
+            }
+        }
+    }
+}
