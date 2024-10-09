88779c52b113dc5f615912b1f922a1b946053ee4
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-10-09 12:38:03 +0300

    Add LoginServiceProtocol.swift

--- Task-App/Login/Service/LoginServiceProtocol.swift
+++ Task-App/Login/Service/LoginServiceProtocol.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+
+/// A protocol that defines the requirements for a login service.
+///
+/// The protocol defines necessary properties for encoding and decoding json files.
+/// It also provide a method used to login the user.
+protocol LoginServiceProtocol {
+    // MARK: Properties
+    
+    /// Property used to decode json data.
+    var decoder: JSONDecoder {get}
+    /// Property used to encode json data.
+    var encoder: JSONEncoder {get}
+    
+    // MARK: Methods 
+    
+    /// Method that will login the user.
+    ///
+    /// It will send the email and the password to the server.
+    /// If the the credentials are correct it will return JWT
+    /// otherwise it will thrown an error.
+    ///
+    /// - Parameters:
+    ///   - email: The email of the user.
+    ///   - password: The password of the user.
+    /// - Returns: The token returned by the server.
+    func login(email: String, password: String) async throws -> String
+}
