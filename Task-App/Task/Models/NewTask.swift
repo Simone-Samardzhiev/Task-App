e68922ae06497dc0e25125f5237a539e9ff50bde
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-10-11 00:30:16 +0300

    Add Task.swift and NewTask.swift

--- Task-App/Task/Models/NewTask.swift
+++ Task-App/Task/Models/NewTask.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+
+/// Struct representing a task that was created.
+///
+/// It is used to send new tasks to the server.
+struct NewTask: Encodable {
+    /// The name of the task.
+    let name: String
+    /// The description of the task.
+    let description: String
+    /// The priority of the task.
+    let priority: Priority
+    /// The date till the task has to be completed.
+    let dueDate: Date
+}
--- Task-App/Task/Models/Task.swift
+++ Task-App/Task/Models/Task.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+
+/// Struct representing a task.
+///
+/// It is used to parse the json data send by the server and also to
+/// display it on the UI.
+struct Task: Codable, Hashable {
+    /// The id of the task.
+    let id: UUID
+    /// The name of the task.
+    var name: String
+    /// The description of the task.
+    var description: String
+    /// The priority of the task.
+    var priority: Priority
+    /// The date till the task has to be completed.
+    var dueDate: Date
+    /// Date when the task was deleted.
+    var dateDeleted: Date?
+    /// Date when the task was completed.
+    var dateCompleted: Date?
+}
