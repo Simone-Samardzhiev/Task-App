8a1f353fb1837eb0c79f731a060d497cc4edfb28
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-10-10 11:33:50 +0300

    Add missing documentation to RegisterError

--- Task-App/Register/Errors/RegisterError.swift
+++ Task-App/Register/Errors/RegisterError.swift
@@ -8,6 +8,17 @@
 import Foundation
 
 
+/// Enumeration representing the errors that may be thrown during user registering process.
+///
+/// - emailInUse: The provided email is already used by another user.
+/// - unknownError: An unspecified error occurred, typically used as a fallback.
+/// - errorEncodingData: Failed to encode user data for the request.
+/// - invalidURL: The URL for the login endpoint is invalid.
+/// - notConnected: The device is not connected to the internet.
+/// - invalidURLResponse: The response received is not a valid URL response.
+/// - invalidHTTPStatusCode: The HTTP status code in not recognized or expected.
+/// - invalidDataResponse: The data cannot be interpreted as expected
+
 enum RegisterError : Error {
     case emailInUse
     case unknownError
