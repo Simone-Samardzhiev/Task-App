9089faecd84b3c0ab904710a37e8d8f57e459b01
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-10-11 10:53:33 +0300

    Add TaskServiceProtocol.swift

--- Task-App/Task/Services/TaskServiceProtocol.swift
+++ Task-App/Task/Services/TaskServiceProtocol.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+
+/// Protocol that defines the requirements for Task service.
+///
+/// The protocol defines properties for encoding and decoding json files.
+/// It also provide property that will store the JWT.
+///
+/// It defines method for getting, adding, deleting and updating a task.
+protocol TaskServiceProtocol {
+    var decoder: JSONDecoder { get }
+    var encoder: JSONEncoder { get }
+    var token: String { get }
+    
+    func getTasks() async throws -> [TaskItem]
+    func addTask(_ task: NewTaskItem) async throws
+    func deleteTask(_ task: TaskItem) async throws
+    func updateTask(_ task: TaskItem) async throws
+}
