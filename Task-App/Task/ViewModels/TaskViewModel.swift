ad4d881e8b34ed8fa27e66c86a0769b45dfbb543
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-10-12 15:08:57 +0300

    Add TaskViewModel

--- Task-App/Task/ViewModels/TaskViewModel.swift
+++ Task-App/Task/ViewModels/TaskViewModel.swift
@@ -6,3 +6,171 @@
 //
 
 import Foundation
+import SwiftUI
+
+@Observable
+class TaskViewModel: TaskViewModelProtocol {
+    // MARK: Properties
+    
+    var uncompletedTasks: [TaskItem]
+    var completedTasks: [TaskItem]
+    var deletedTasks: [TaskItem]
+    var state: ProgressState
+    @ObservationIgnored var refreshTokenTask: Task<Void, any Error>?
+    @ObservationIgnored let closeTaskView: (ProgressState) -> Void
+    @ObservationIgnored let service: TaskServiceProtocol
+    
+    // MARK: Initializer
+    
+    init(closeTaskView: @escaping (ProgressState) -> Void, service: TaskServiceProtocol) {
+        self.uncompletedTasks = []
+        self.completedTasks = []
+        self.deletedTasks = []
+        self.state = .idle
+        self.refreshTokenTask = nil
+        self.closeTaskView = closeTaskView
+        self.service = service
+    }
+    
+    func getTasks() async {
+        await MainActor.run {
+            changeState(.processing("Loading tasks"))
+        }
+        
+        do {
+            let tasks = try await service.getTasks()
+            await MainActor.run {
+                for task in tasks {
+                    if task.dateDeleted != nil {
+                        deletedTasks.append(task)
+                    } else if task.dateCompleted != nil {
+                        completedTasks.append(task)
+                    } else {
+                        uncompletedTasks.append(task)
+                    }
+                }
+                
+                changeState(.idle)
+            }
+        } catch let error as TaskError {
+            await MainActor.run {
+                handleTaskError(error)
+            }
+        } catch {
+            await MainActor.run {
+                handleUnknownError(error)
+            }
+        }
+    }
+    
+    func addTask(_ task: NewTaskItem) async {
+        await MainActor.run {
+            changeState(.processing("Adding task"))
+        }
+        
+        do {
+            try await service.addTask(task)
+            await MainActor.run {
+                changeState(.success("Task added!"))
+            }
+        } catch let error as TaskError {
+            await MainActor.run {
+                handleTaskError(error)
+            }
+        } catch {
+            await MainActor.run {
+                handleUnknownError(error)
+            }
+        }
+    }
+    
+    func updateTask(_ task: TaskItem) async {
+        await MainActor.run {
+            changeState(.processing("Updating task"))
+        }
+        
+        do {
+            try await service.updateTask(task)
+            await MainActor.run {
+                changeState(.success("Task updated!"))
+            }
+        } catch let error as TaskError {
+            await MainActor.run {
+                handleTaskError(error)
+            }
+        } catch {
+            await MainActor.run {
+                handleUnknownError(error)
+            }
+        }
+    }
+    
+    func deleteTask(_ task: TaskItem) async {
+        await MainActor.run {
+            changeState(.processing("Deleting task"))
+        }
+        
+        do {
+            try await service.updateTask(task)
+            await MainActor.run {
+                changeState(.success("Task deleted!"))
+            }
+        } catch let error as TaskError {
+            await MainActor.run {
+                handleTaskError(error)
+            }
+        } catch {
+            await MainActor.run {
+                handleUnknownError(error)
+            }
+        }
+    }
+    
+    func handleTaskError(_ error: TaskError) {
+        switch error {
+        case .notConnected:
+            changeState(.failure("Check your connection!"))
+        case .tokenExpired:
+            closeTaskView(.failure("Token expired! Please login again!"))
+        default:
+            closeTaskView(.failure("There was an unknown error!"))
+#if DEBUG
+            print(error)
+#endif
+        }
+    }
+    
+    func handleUnknownError(_ error: any Error) {
+        closeTaskView(.failure("There was an unknown error!"))
+#if DEBUG
+        print(error)
+#endif
+    }
+    
+    func changeState(_ newState: ProgressState) {
+        withAnimation {
+            state = newState
+        }
+    }
+    
+    func startRefreshTokenTask() {
+        refreshTokenTask = Task {
+            try? await Task.sleep(for: .seconds(240))
+            do {
+                try await service.refreshToken()
+            } catch let error as TaskError {
+                await MainActor.run {
+                    handleTaskError(error)
+                }
+            } catch {
+                await MainActor.run {
+                    handleUnknownError(error)
+                }
+            }
+        }
+    }
+    
+    func stopRefreshTokenTask() {
+        refreshTokenTask?.cancel()
+    }
+}
