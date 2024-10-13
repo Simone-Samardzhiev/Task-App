f7734b176b24593b63f9884a08189f95797dd019
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-10-13 16:07:57 +0300

    Add TaskType.swift

--- Task-App/Task/Enums/TaskType.swift
+++ Task-App/Task/Enums/TaskType.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+
+/// Enumeration defining type of tasks.
+enum TaskType: String {
+    case uncompleted = "Uncompleted tasks"
+    case completed = "Completed tasks"
+    case deleted = "Deleted tasks"
+}
